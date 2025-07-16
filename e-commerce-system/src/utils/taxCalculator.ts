951e456dfcab8d7ea5dfa2848a7e10404d825b99
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-16 16:25:03 -0400

    Implemented taxCalculator.

--- e-commerce-system/src/utils/taxCalculator.ts
+++ e-commerce-system/src/utils/taxCalculator.ts
@@ -1,8 +1,16 @@
+import Product from "../models/Product"
+
 const DEFAULT_TAX_RATE: number = 0.0475
 const GROCERIES_TAX_RATE: number = 0.03
 
-function calculateTax() {
-  /* TODO */
+function productIsGroceries(product: Product): boolean {
+  return product.category === "groceries"
+}
+
+function calculateTax(product: Product) {
+  return productIsGroceries(product)
+    ? product.price * GROCERIES_TAX_RATE
+    : product.price * DEFAULT_TAX_RATE
 }
 
 export default calculateTax
