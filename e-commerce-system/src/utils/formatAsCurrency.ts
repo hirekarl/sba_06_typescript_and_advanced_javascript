5cbcefcaf4703de5c5745485200be05b36f1828a
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-16 16:24:45 -0400

    Moved formatAsCurrency to utils.

---
+++ e-commerce-system/src/utils/formatAsCurrency.ts
@@ -0,0 +1,10 @@
+function formatAsCurrency(amount: number): string {
+  return amount.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
+}
+
+export default formatAsCurrency
